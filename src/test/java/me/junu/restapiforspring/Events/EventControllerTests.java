4fd9147f39253386de5ad9865c4ac59e73bd8475
junwoochoi <junu7370!#>
2019-01-16 22:12:36 +0900

    Create TestDescription Annotation

--- src/test/java/me/junu/restapiforspring/Events/EventControllerTests.java
+++ src/test/java/me/junu/restapiforspring/Events/EventControllerTests.java
@@ -1,6 +1,7 @@
 package me.junu.restapiforspring.Events;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import me.junu.restapiforspring.common.TestDescription;
 import org.hamcrest.Matchers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -33,6 +34,7 @@ public class EventControllerTests {
     ObjectMapper objectMapper;
 
     @Test
+    @TestDescription("정상적으로 이벤트를 생성하는 테스트")
     public void createEvent() throws Exception {
         EventDto event = EventDto.builder()
                 .name("Spring")
@@ -62,6 +64,7 @@ public class EventControllerTests {
 
 
     @Test
+    @TestDescription("입력받을 수 없는 값을 사용한 경우에 에러가 발생하는 테스트")
     public void createEvent_Bad_Request() throws Exception {
         Event event = Event.builder()
                 .name("Spring")
@@ -89,6 +92,7 @@ public class EventControllerTests {
     }
 
     @Test
+    @TestDescription("입력값이 비어있는 경우에 에러가 발생하는 테스트")
     public void createEvent_Bad_Request_Empty_Input() throws Exception {
         EventDto eventDto = EventDto.builder().build();
 
@@ -100,6 +104,7 @@ public class EventControllerTests {
     }
 
     @Test
+    @TestDescription("입력값이 잘못된 경우에 에러가 발생하는 테스트")
     public void createEvent_Bad_Request_Wrong_Input() throws Exception {
         EventDto eventDto = EventDto.builder()
                 .name("Spring")
