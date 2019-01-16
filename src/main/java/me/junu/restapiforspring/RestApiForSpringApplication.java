558d6aca6fdeb61db83a59188d4b65db3e689b70
최준우 <cjw4690@gmail.com>
2019-01-16 13:47:19 +0900

    Initial commit

---
+++ src/main/java/me/junu/restapiforspring/RestApiForSpringApplication.java
@@ -0,0 +1,14 @@
+package me.junu.restapiforspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiForSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestApiForSpringApplication.class, args);
+    }
+
+}
+
