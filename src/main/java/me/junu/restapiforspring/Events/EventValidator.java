8e0ca358dd7a619a774cd312f083c2b980fd26cf
최준우 <cjw4690@gmail.com>
2019-01-17 15:06:46 +0900

    refactor EventValidator

--- src/main/java/me/junu/restapiforspring/Events/EventValidator.java
+++ src/main/java/me/junu/restapiforspring/Events/EventValidator.java
@@ -9,19 +9,38 @@ import java.time.LocalDateTime;
 @Component
 public class EventValidator {
     public void validate(EventDto eventDto, Errors errors){
-        if(eventDto.getBasePrice()>eventDto.getMaxPrice() && eventDto.getMaxPrice() > 0){
-            errors.rejectValue("basePrice", "wrongValue", "BasePrice is wrong");
-            errors.rejectValue("maxPrice", "wrongValue", "MaxPrice is wrong");
+        validatePrice(eventDto, errors);
+        validateEventDateTime(eventDto, errors, eventDto.getEndEventDateTime());
+        validateBeginEventDateTime(eventDto, errors, eventDto.getBeginEventDateTime());
+        validateCloseEnrollmentDateTime(eventDto, errors, eventDto.getCloseEnrollmentDateTime());
+    }
+
+    private void validateCloseEnrollmentDateTime(EventDto eventDto, Errors errors, LocalDateTime closeEnrollmentDateTime) {
+        if(closeEnrollmentDateTime.isBefore(eventDto.getBeginEnrollmentDateTime())){
+            errors.rejectValue("closeEnrollmentDateTime", "wrongValue", "closeEnrollmentDateTime is wrong");
         }
+    }
 
-        LocalDateTime endEventDateTime = eventDto.getEndEventDateTime();
+    private void validateBeginEventDateTime(EventDto eventDto, Errors errors, LocalDateTime beginEventDateTime) {
+        if(beginEventDateTime.isBefore(eventDto.getBeginEnrollmentDateTime()) ||
+        beginEventDateTime.isBefore(eventDto.getCloseEnrollmentDateTime()) ||
+        beginEventDateTime.isAfter(eventDto.getEndEventDateTime())){
+            errors.rejectValue("beginEventDateTime", "wrongValue","beginEventDateTime is wrong");
+        }
+    }
 
+    private void validateEventDateTime(EventDto eventDto, Errors errors, LocalDateTime endEventDateTime) {
         if(endEventDateTime.isBefore(eventDto.getBeginEventDateTime()) ||
         endEventDateTime.isBefore(eventDto.getCloseEnrollmentDateTime()) ||
         endEventDateTime.isBefore(eventDto.getBeginEnrollmentDateTime())) {
             errors.rejectValue("endEventDateTime", "wrongValue", "endEventDateTime is wrong");
         }
-        //TODO BeginEventDateTime
-        //TODO CloseEnrollmentDateTime
+    }
+
+    private void validatePrice(EventDto eventDto, Errors errors) {
+        if(eventDto.getBasePrice()>eventDto.getMaxPrice() && eventDto.getMaxPrice() > 0){
+            errors.rejectValue("basePrice", "wrongValue", "BasePrice is wrong");
+            errors.rejectValue("maxPrice", "wrongValue", "MaxPrice is wrong");
+        }
     }
 }
