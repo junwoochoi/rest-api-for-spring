1f44d5435cf53db7ee35e8b3f6d151ba8508b241
junwoochoi <cjw4690@gmail.com>
2019-01-25 15:53:19 +0900

    make events controller to send index links when send errors

--- src/main/java/me/junu/restapiforspring/events/EventController.java
+++ src/main/java/me/junu/restapiforspring/events/EventController.java
@@ -1,5 +1,6 @@
 package me.junu.restapiforspring.events;
 
+import me.junu.restapiforspring.common.ErrorsResource;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.Link;
@@ -32,13 +33,13 @@ public class EventController {
     @PostMapping
     public ResponseEntity createEvent(@RequestBody @Valid EventDto eventDto, Errors errors) {
         if(errors.hasErrors()) {
-            return ResponseEntity.badRequest().body(errors);
+            return badRequest(errors);
         }
 
         eventValidator.validate(eventDto, errors);
 
         if(errors.hasErrors()){
-            return ResponseEntity.badRequest().body(errors);
+            return badRequest(errors);
         }
 
         Event event = modelMapper.map(eventDto, Event.class);
@@ -55,6 +56,10 @@ public class EventController {
         eventResource.add(new Link("/docs/index.html#resources-events-create", "profile"));
 
         return ResponseEntity.created(createdUri).body(eventResource);
+    }
+
+    private ResponseEntity<ErrorsResource> badRequest(Errors errors) {
+        return ResponseEntity.badRequest().body(new ErrorsResource(errors));
     }
 
 }
--- src/test/java/me/junu/restapiforspring/events/EventControllerTests.java
+++ src/test/java/me/junu/restapiforspring/events/EventControllerTests.java
@@ -181,8 +181,9 @@ public class EventControllerTests {
                 .content(objectMapper.writeValueAsString(eventDto)))
                 .andDo(print())
                 .andExpect(status().isBadRequest())
-                .andExpect(jsonPath("$[0].objectName").exists())
-                .andExpect(jsonPath("$[0].defaultMessage").exists())
-                .andExpect(jsonPath("$[0].code").exists());
+                .andExpect(jsonPath("content[0].objectName").exists())
+                .andExpect(jsonPath("content[0].defaultMessage").exists())
+                .andExpect(jsonPath("content[0].code").exists())
+                .andExpect(jsonPath("_links.index").exists());
     }
 }
