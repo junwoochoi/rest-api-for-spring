a32b0ba633825786bc9662311cdebb5dad864559
junwoochoi <cjw4690@gmail.com>
2019-01-29 14:44:58 +0900

    create ErrorResource.class

---
+++ src/main/java/me/junu/restapiforspring/common/ErrorsResource.java
@@ -0,0 +1,17 @@
+package me.junu.restapiforspring.common;
+
+import me.junu.restapiforspring.index.IndexController;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.Resource;
+import org.springframework.validation.Errors;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
+
+
+public class ErrorsResource extends Resource {
+    public ErrorsResource(Errors content, Link... links) {
+        super(content, links);
+        add(linkTo(methodOn(IndexController.class).index()).withRel("index"));
+    }
+}
